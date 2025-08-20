512de5e5d89b161c81fee5719caa966b94670611
andyavl <andyavl@hotmail.com>
2025-08-20 14:17:35 -0400

    1. Block Scope

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,4 +1,4 @@
-export default function taskBlock(trueOrFalse) {
+export default function taskBlock (trueOrFalse) {
   const task = false;
   const task2 = true;
 
@@ -6,7 +6,7 @@ export default function taskBlock(trueOrFalse) {
     /* yet another documentation for checker */
     const task = true;
     const task2 = false;
-    /* it whines but doesnt say what the problem is */ 
+    /* it whines but doesnt say what the problem is */
   }
 
   return [task, task2];
