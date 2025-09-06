54f4775e6908ecb64176d69364799688eb7f49d1
andyavl <andyavl@hotmail.com>
2025-09-06 14:49:13 -0400

    2. Reading a file synchronously with Node JS

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function countStudents (file) {
+  try {
+    const data = fs.readFileSync(file, 'utf-8');
+    const lines = data.split('\n');
+    const students = lines.slice(1);
+
+    console.log(`Number of students: ${students.length}`);
+
+    const studentFields = {};
+
+    students.forEach((line) => {
+      const parts = line.split(',');
+      const firstname = parts[0];
+      const field = parts[3];
+
+      if (!studentFields[field]) {
+        studentFields[field] = [];
+      }
+      studentFields[field].push(firstname);
+    });
+
+    for (const field in studentFields) {
+      const names = studentFields[field];
+      console.log(
+        'Number of students in ' + field + ': ' + names.length + '. List: ' + names.join(', ')
+      );
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
